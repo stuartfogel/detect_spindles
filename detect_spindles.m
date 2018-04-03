413ac05a349b022b79b27151130852821961ac13
Stuart Fogel <sfogel@uwo.ca>
2018-04-03 10:53:52 -0400

    Update detect_spindles.m
    updated custom parameters to avoid error on newer versions of Matlab

--- detect_spindles.m
+++ detect_spindles.m
@@ -39,6 +39,31 @@ addpath(genpath('./eeglab13_2_2b'))
 if nargin < 1; EEG = []; end
 if nargin < 2; PARAM.emptyparam = 1; end
 
+%% CUSTOM PARAMETERS
+if PARAM.emptyparam == 1
+    PARAM = struct(...
+        'PB_forder',1 ... order for the first low pass filter. Default [1].
+        ,'cdemod_freq', 13.5 ...    central frequency (i.e. Carrier frequency in Hz) for the complex demodulation. Default: [13.5].
+        ,'cdemod_filter_lowpass', 5 ... cutoff frequency for the low pass filter in CD. Default: [5].
+        ,'cdemod_forder', 4 ...    filter order for the complex demodulation. Default: [4].
+        ,'channels_of_interest',{{'C3','O1'}} ... selected channels. Default: {{'Fz','Cz','Pz'}}
+        ,'ZSwindowlength', 60 ... window for ZSCORE (in seconds). Default: [60].
+        ,'ZSThreshold', 2.33 ... Threshold for the ZScore. Default: [2.33].
+        ,'ZSResetThreshold', 0.1 ... Value for the reset. Default: [0.25].
+        ,'ZSBeginThreshold', 0.1 ... Value to detect the begining of spindles. Default: [0.25].
+        ,'ZSDelay', 0.25 ... minimum delay btw 2 spindles on the same channel (sec.). Default: [0.25].
+        ,'minDur', 0.49 ... minimum spindle duration. Default: [0.25].
+        ,'eventName', {{'Spindle'}} ... name of event. Default: {{'Spindle'}}.
+        ,'allsleepstages', {{'NREM 1','NREM 2','NREM 3','NREM 4','REM','Wake','unscored'}} ... name of all sleep stage markers. Default: {{'N1','N2','N3','N4','REM','W','unscored'}}.
+        ,'goodsleepstages', {{'NREM 2','NREM 3','NREM 4'}} ... name of sleep stage markers to keep spindle events. Default: {{'N2','N3','N4'}}.
+        ,'badData', {{'Movement'}} ... name for movement artifact. Default: {{'Movement'}}.
+        ,'save_result_file','.csv' ... file type to save markers to a file. If empty []: popup window. Default: {{'.csv'}}
+        ,'save_mat_file', 1 ... set to = 1 to save EEG, markers and PARAM to a .mat file. If empty []: popup window. Default: [1].
+        ,'output_allfiles', 0 ... set at 1 if you want the complete results with all steps. Note EEG struct will be different dimentions. Default: [0].
+        ,'emptyparam', 0 ... set PARAM.emptyparam to not empty.
+        );
+end
+
 %% Specify Filename(s)
 % you can manually specify filenames here, or leave empty for pop-up
 pathname = '';
@@ -107,30 +132,6 @@ for nfile = 1:length(filename)
     EEG.filepath = [resultDir,filesep];
     OutputPath = [resultDir,filesep];
     OutputFile = EEG.setname;
-    
-    %% CUSTOM PARAMETERS
-    if PARAM.emptyparam == 1
-        PARAM = struct(...
-            'PB_forder',1 ... order for the first low pass filter. Default [1].
-            ,'cdemod_freq', 13.5 ...    central frequency (i.e. Carrier frequency in Hz) for the complex demodulation. Default: [13.5].
-            ,'cdemod_filter_lowpass', 5 ... cutoff frequency for the low pass filter in CD. Default: [5].
-            ,'cdemod_forder', 4 ...    filter order for the complex demodulation. Default: [4].
-            ,'channels_of_interest',{{'Fz','Cz','Pz'}} ... selected channels. Default: {{'Fz','Cz','Pz'}}
-            ,'ZSwindowlength', 60 ... window for ZSCORE (in seconds). Default: [60].
-            ,'ZSThreshold', 2.33 ... Threshold for the ZScore. Default: [2.33].
-            ,'ZSResetThreshold', 0.1 ... Value for the reset. Default: [0.1].
-            ,'ZSBeginThreshold', 0.1 ... Value to detect the begining of spindles. Default: [0.1].
-            ,'ZSDelay', 0.25 ... minimum delay btw 2 spindles on the same channel (sec.). Default: [0.25].
-            ,'minDur', 0.49 ... minimum spindle duration. Default: [0.49].
-            ,'eventName', {{'Spindle'}} ... name of event. Default: {{'Spindle'}}.
-            ,'allsleepstages', {{'N1','N2','N3','N4','REM','W','unscored'}} ... name of all sleep stage markers. Default: {{'N1','N2','N3','N4','REM','W','unscored'}}.
-            ,'goodsleepstages', {{'N2','N3','N4'}} ... name of sleep stage markers to keep spindle events. Default: {{'N2','N3','N4'}}.
-            ,'badData', {{'Movement'}} ... name for movement artifact. Default: {{'Movement'}}.
-            ,'save_result_file','.csv' ... file type to save markers to a file. If empty []: popup window. Default: {{'.csv'}}
-            ,'save_mat_file', 1 ... set to = 1 to save EEG, markers and PARAM to a .mat file. If empty []: popup window. Default: [1].
-            ,'output_allfiles', 0 ... set at 1 if you want the complete results with all steps. Note EEG struct will be different dimentions. Default: [0].
-            );
-    end
     
     %% real pipeline
     if length(EEG)>1
