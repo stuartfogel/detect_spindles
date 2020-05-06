7e5ba4b7961c67095b0398ad9d8c3e8bf08668e9
Stuart Fogel <sfogel@uottawa.ca>
2020-05-06 08:43:45 -0400

    adapted to be compatible eeglab plugin

--- src/DS_pipeline_detect_spindles.m
+++ src/DS_pipeline_detect_spindles.m
@@ -1,4 +1,4 @@
-function [EEG, marker, PARAM] = DS_pipeline_detect_spindles(EEG,PARAM,OutputFile,OutputPath)
+function [EEG] = DS_pipeline_detect_spindles(EEG,PARAM)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -8,7 +8,6 @@ function [EEG, marker, PARAM] = DS_pipeline_detect_spindles(EEG,PARAM,OutputFile
 %           PARAM = structure of parameters. See detect_spindles.m.
 %
 % OUTPUT:   EEG = same structure with spindle markers (EEG.event)
-%           marker = spindle markers
 %
 % Authors:  Stephane Sockeel, PhD, University of Montreal
 %           Stuart Fogel, PhD, University of Ottawa
@@ -31,24 +30,12 @@ function [EEG, marker, PARAM] = DS_pipeline_detect_spindles(EEG,PARAM,OutputFile
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% just for debug, or if you need to keep each processing step :
-Output_All = 0;
-if isfield(PARAM,'output_allfiles')
-    if PARAM.output_allfiles
-        Output_All = 1;
-    end
-end
-
 %% 0) Start pipeline
 t0 = clock; % start time
 fprintf(1,'%s\n',['------------------------ ' datestr(t0) ' ------------------------']);
 
 % Check data format for compatibility. Ensure EEG.data are double (filtfilt requirement). This also resolves other related issues.
 EEG.data = double(EEG.data);
-if Output_All
-    ALLEEG(1) = EEG;
-    ALLEEG(1).setname = 'raw data';
-end
 
 %% 1) COMPLEX DEMODULATION OR RMS
 
@@ -62,19 +49,11 @@ if PARAM.cdemodORrms == 1
     fprintf(1,'%s\n',' ');
     fprintf(1,'%s\n','STEP 1: COMPLEX DEMODULATION');
     EEGfreq = DS_complexDemodulation(EEGcoi,PARAM);
-    if Output_All
-        ALLEEG(2) = EEGfreq;
-        ALLEEG(2).setname = 'ComplexDemod';
-    end
-% 1c) if RMS is your weapon of choice
+    % 1c) if RMS is your weapon of choice
 elseif PARAM.cdemodORrms == 0
     fprintf(1,'%s\n',' ');
     fprintf(1,'%s\n','STEP 1: ROOT MEAN SQUARE TRANSFORMATION');
     EEGfreq = DS_rms(EEGcoi,PARAM);
-    if Output_All
-        ALLEEG(2) = EEGfreq;
-        ALLEEG(2).setname = 'RMS';
-    end
 else
     error('Please choose either complex demodulate or RMS to extract frequencies of interest.')
 end
@@ -87,15 +66,11 @@ fprintf(1,'%s\n',[' ~~ ' num2str(etime(t1,t0)) ' sec.']);
 fprintf(1,'%s\n',' ');
 fprintf(1,'%s\n','STEP 2: Z-SCORE NORMALIZATION');
 
-% 2a) set data during movements to NaN so they don't contaminate normalization 
+% 2a) set data during movements to NaN so they don't contaminate normalization
 EEGnan = DS_NaNbadData(EEGfreq,PARAM);
 
 % 2b) Signal normalization
 EEGz = DS_Zscore_new(EEGnan,PARAM);
-if Output_All
-    ALLEEG(3) = EEGz;
-    ALLEEG(3).setname = 'ZScore';
-end
 
 t2 = clock;
 fprintf(1,'%s\n',[' ~~ ' num2str(etime(t2,t1)/60) ' min.']);
@@ -110,19 +85,10 @@ EEGs.urevent = [];
 EEGs = EEGz;
 
 % detect spindles
-[EEGs, marker] = DS_Threshold(EEGs,PARAM);
-if Output_All
-    ALLEEG(4) = EEGs;
-    ALLEEG(4).setname = 'ZScore_with_spindles';
-end
+[EEGs] = DS_Threshold(EEGs,PARAM);
 
 % spindle merge events back with existing events and original data:
 EEG = DS_merge_event(EEG,EEGs);
-
-if Output_All
-    ALLEEG(5) = EEG;
-    ALLEEG(5).setname = 'Raw_Data with Spindles';
-end
 
 t3 = clock;
 fprintf(1,'%s\n',[' ~~ ' num2str(etime(t3,t2)/60) ' min.']);
@@ -156,11 +122,6 @@ fprintf(1,'%s\n','STEP 6: SPINDLE CHARACTERIZATION');
 
 EEG = DS_characSpindles(EEG, PARAM);
 
-if Output_All
-    ALLEEG(6) = EEG;
-    ALLEEG(6).setname = 'Final product';
-end
-
 t6 = clock;
 fprintf(1,'%s\n',[' ~~ ' num2str(etime(t6,t5)/60) ' min.']);
 
@@ -169,21 +130,12 @@ fprintf(1,'%s\n',' ');
 fprintf(1,'%s\n','STEP 7: EXPORT SPINDLE MARKERS');
 
 if isfield(PARAM,'save_result_file')
-    if isempty(PARAM.save_result_file)
-        [OutputFile, OutputPath] = uiputfile({'*.csv','CSV Files (*.csv)';'*.*','All Files'},'Export MarkerFiles ?');
-        if OutputFile ~= 0
-            export_Spindles_csv([OutputPath OutputFile '.csv'],EEG);
-        end
-    elseif ~isempty(PARAM.save_result_file)
-        export_Spindles_csv([OutputPath OutputFile '.csv'],EEG);
+    if ~isempty(PARAM.save_result_file)
+        export_Spindles_csv([EEG.filepath EEG.setname '.csv'],EEG);
     end
 end
 
 t7 = clock;
 fprintf(1,'%s\n',[' ~~ ' num2str(etime(t7,t6)/60) ' min.']);
-
-if Output_All
-    EEG = ALLEEG;
-end
 
 end
