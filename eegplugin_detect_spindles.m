d83a658bde7562429d946d4ab3276ad6fc529461
Stuart Fogel <sfogel@uottawa.ca>
2020-05-06 08:56:04 -0400

    Fixed bug to properly add subdirectories to path

--- eegplugin_detect_spindles.m
+++ eegplugin_detect_spindles.m
@@ -33,10 +33,10 @@ end
 % add plugin folder to path
 % -----------------------
 if exist('pop_detect_spindles.m','file')
-    p = which('eegplugin__detect_spindles');
+    p = which('eegplugin_detect_spindles');
     p = p(1:findstr(p,'eegplugin_detect_spindles.m')-1);
     addpath(p);
-    addpath([p 'src'])
+    addpath([p 'src/'])
 end
 
 % find tools menu
